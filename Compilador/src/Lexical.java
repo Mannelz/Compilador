89d6d7970eab8eca27c7fc33ef05927dd930a0b7
Mannelz <emannuelm29@gmail.com>
2025-04-15 11:18:28 -0300

    move create table

--- Compilador/src/Lexical.java
+++ Compilador/src/Lexical.java
@@ -5,46 +5,6 @@ import java.util.Map;
 
 public class Lexical
 {
-    public static Map<String, Simbolo> CreateTable() 
-    {
-        Map<String, Simbolo> symbolsTable = new HashMap<>();
-
-        symbolsTable.put("final", new Simbolo("FINAL", "3", "final"));
-        symbolsTable.put("int", new Simbolo("INT", "4", "int"));
-        symbolsTable.put("byte", new Simbolo("BYTE", "5", "byte"));
-        symbolsTable.put("string", new Simbolo("STRING", "6", "string"));
-        symbolsTable.put("while", new Simbolo("WHILE", "7", "while"));
-        symbolsTable.put("if", new Simbolo("IF", "8", "if"));
-        symbolsTable.put("else", new Simbolo("ELSE", "9", "else"));
-        symbolsTable.put("and", new Simbolo("AND", "10", "and"));
-        symbolsTable.put("or", new Simbolo("OR", "11", "or"));
-        symbolsTable.put("not", new Simbolo("NOT", "12", "not"));
-        symbolsTable.put("==", new Simbolo("IGUAL", "13", "=="));
-        symbolsTable.put("=", new Simbolo("ATRIB", "14", "="));
-        symbolsTable.put("(", new Simbolo("ABRE_PAR", "15", "("));
-        symbolsTable.put(")", new Simbolo("FECHA_PAR", "16", ")"));
-        symbolsTable.put("<", new Simbolo("MENOR", "17", "<"));
-        symbolsTable.put(">", new Simbolo("MAIOR", "18", ">"));
-        symbolsTable.put(">=", new Simbolo("MAIOR_IGUAL", "19", ">="));
-        symbolsTable.put("<=", new Simbolo("MENOR_IGUAL", "20", "<="));
-        symbolsTable.put(",", new Simbolo("VIRGULA", "21", ","));
-        symbolsTable.put("+", new Simbolo("SOMA", "22", "+"));
-        symbolsTable.put("-", new Simbolo("SUB", "23", "-"));
-        symbolsTable.put("*", new Simbolo("MULT", "24", "*"));
-        symbolsTable.put("/", new Simbolo("DIV", "25", "/"));
-        symbolsTable.put(";", new Simbolo("PONTO_VIRG", "26", ";"));
-        symbolsTable.put("begin", new Simbolo("BEGIN", "27", "begin"));
-        symbolsTable.put("end", new Simbolo("END", "28", "end"));
-        symbolsTable.put("readln", new Simbolo("READLN", "29", "readln"));
-        symbolsTable.put("write", new Simbolo("WRITE", "30", "write"));
-        symbolsTable.put("writeln", new Simbolo("WRITELN", "31", "writeln"));
-        symbolsTable.put("true", new Simbolo("TRUE", "32", "true"));
-        symbolsTable.put("false", new Simbolo("FALSE", "33", "false"));
-        symbolsTable.put("boolean", new Simbolo("BOOLEAN", "34", "boolean"));
-
-        return symbolsTable;
-    }
-
     public static void Analysis(String filePath)
     {
         Map<String, Simbolo> symbolsTable = CreateTable();
@@ -144,5 +104,45 @@ public class Lexical
         {
             System.out.println("Error: " + e);
         }
+    }
+
+    public static Map<String, Simbolo> CreateTable() 
+    {
+        Map<String, Simbolo> symbolsTable = new HashMap<>();
+
+        symbolsTable.put("final", new Simbolo("FINAL", "3", "final"));
+        symbolsTable.put("int", new Simbolo("INT", "4", "int"));
+        symbolsTable.put("byte", new Simbolo("BYTE", "5", "byte"));
+        symbolsTable.put("string", new Simbolo("STRING", "6", "string"));
+        symbolsTable.put("while", new Simbolo("WHILE", "7", "while"));
+        symbolsTable.put("if", new Simbolo("IF", "8", "if"));
+        symbolsTable.put("else", new Simbolo("ELSE", "9", "else"));
+        symbolsTable.put("and", new Simbolo("AND", "10", "and"));
+        symbolsTable.put("or", new Simbolo("OR", "11", "or"));
+        symbolsTable.put("not", new Simbolo("NOT", "12", "not"));
+        symbolsTable.put("==", new Simbolo("IGUAL", "13", "=="));
+        symbolsTable.put("=", new Simbolo("ATRIB", "14", "="));
+        symbolsTable.put("(", new Simbolo("ABRE_PAR", "15", "("));
+        symbolsTable.put(")", new Simbolo("FECHA_PAR", "16", ")"));
+        symbolsTable.put("<", new Simbolo("MENOR", "17", "<"));
+        symbolsTable.put(">", new Simbolo("MAIOR", "18", ">"));
+        symbolsTable.put(">=", new Simbolo("MAIOR_IGUAL", "19", ">="));
+        symbolsTable.put("<=", new Simbolo("MENOR_IGUAL", "20", "<="));
+        symbolsTable.put(",", new Simbolo("VIRGULA", "21", ","));
+        symbolsTable.put("+", new Simbolo("SOMA", "22", "+"));
+        symbolsTable.put("-", new Simbolo("SUB", "23", "-"));
+        symbolsTable.put("*", new Simbolo("MULT", "24", "*"));
+        symbolsTable.put("/", new Simbolo("DIV", "25", "/"));
+        symbolsTable.put(";", new Simbolo("PONTO_VIRG", "26", ";"));
+        symbolsTable.put("begin", new Simbolo("BEGIN", "27", "begin"));
+        symbolsTable.put("end", new Simbolo("END", "28", "end"));
+        symbolsTable.put("readln", new Simbolo("READLN", "29", "readln"));
+        symbolsTable.put("write", new Simbolo("WRITE", "30", "write"));
+        symbolsTable.put("writeln", new Simbolo("WRITELN", "31", "writeln"));
+        symbolsTable.put("true", new Simbolo("TRUE", "32", "true"));
+        symbolsTable.put("false", new Simbolo("FALSE", "33", "false"));
+        symbolsTable.put("boolean", new Simbolo("BOOLEAN", "34", "boolean"));
+
+        return symbolsTable;
     }
 }
